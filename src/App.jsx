fdc4e4c7d32647bcfdef42d3db3cd557471a66a9
arth <piyushmishra780720@gmail.com>
2024-06-28 08:03:35 +0530

    add prices section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
+import { Workflow } from 'lucide-react'
+import Price from './components/Price'
 
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection/>
         <FeatureSection/>
+        <Workflow/>
+        <Price/>
       </div>
     </>
   )
